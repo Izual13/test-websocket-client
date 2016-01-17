41f66cf665f5508118ba0a70e9fc1a87c8029087
Maxim Tsoy <mtsoy1990@gmail.com>
2016-01-18 00:51:48 +0400

    update web-socket client

--- src/main/java/com/example/WSClient.java
+++ src/main/java/com/example/WSClient.java
@@ -2,54 +2,73 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.Transport;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 public class WSClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         SpringApplication.run(WSClient.class, args);
         List<Transport> transports = new ArrayList<>(2);
-        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+        StandardWebSocketClient webSocketClient = new StandardWebSocketClient();
+        transports.add(new WebSocketTransport(webSocketClient));
 
 
         SockJsClient sockJsClient = new SockJsClient(transports);
-        sockJsClient.doHandshake(new MyWebSocketHandler(), "ws://localhost:8080/socket");
+        WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+        String url = "ws://localhost:8080/socket";
+        stompClient.connect(url, new StompSessionHandler());
 
     }
 
 
-    private static class MyWebSocketHandler implements WebSocketHandler {
+    static class StompSessionHandler extends StompSessionHandlerAdapter {
         @Override
-        public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+            super.afterConnected(session, connectedHeaders);
+            session.subscribe("/topic", new StringHandler());
+            System.out.println("connected");
         }
 
         @Override
-        public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+            super.handleException(session, command, headers, payload, exception);
+
+            exception.printStackTrace();
+        }
+
+    }
+
+    static class StringHandler extends StompSessionHandlerAdapter {
+        @Override
+        public Type getPayloadType(StompHeaders headers) {
+            return byte[].class;
         }
 
         @Override
-        public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+            super.handleException(session, command, headers, payload, exception);
+            System.out.println("exception: " + exception.getMessage());
         }
 
         @Override
-        public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
-        }
-
-        @Override
-        public boolean supportsPartialMessages() {
-            return false;
+        public void handleFrame(StompHeaders headers, Object payload) {
+            super.handleFrame(headers, payload);
+            System.out.println(new String((byte[]) payload));
         }
     }
 }
